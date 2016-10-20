0122d58b1ea9b69a9b2719f576fcaaf763db7341
cs-canigo <oficina-tecnica.canigo.ctti@gencat.cat>
2016-10-20 10:23:45 +0200

    new version

--- _tasks/algolia/index-algolia.js
+++ _tasks/algolia/index-algolia.js
@@ -14,10 +14,10 @@ algolia.setSettings({
 	'removeStopWords':[true,'ca']
 });
 
-var _indexDir = "./";
+var _indexDir = "../../";
 
 var indexSetup = {
-	"dir" : "../content/",
+	"dir" : "../../content/",
 	"index_empty_content" : true,
 	"excludeIfProps" : ["enllac","no_index"],
 	"excludes" : [
