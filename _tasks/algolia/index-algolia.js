46e68b1279b5480e605fa06ea415f2dbc10a60d5
David Ayala <davixyz@gmail.com>
2016-10-26 09:32:42 +0200

    [ci skip]

--- _tasks/algolia/index-algolia.js
+++ _tasks/algolia/index-algolia.js
@@ -35,6 +35,7 @@ if (!fs.existsSync(_indexDir)){
 
 //gets current index
 request('https://raw.githubusercontent.com/cs-canigo/portal/gh-pages/index.json', function (error, response, body) {
+  fs.unlinkSync(_indexDir+"index.json");
   if (!error && response.statusCode == 200) {
 	fs.writeFileSync(_indexDir+"index.json", body);
   }else{
