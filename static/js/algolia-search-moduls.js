099d0ee87236be03199934313e0f487cae9ad287
Arnau Oncins Rodríguez <arnau.oncins@gmail.com>
2017-01-28 18:09:44 +0100

    Canviat l'APPID i l'APIKEY

--- static/js/algolia-search-moduls.js
+++ static/js/algolia-search-moduls.js
@@ -5,8 +5,8 @@ var search, results = {}, facet_data={};
 
 /* global instantsearch */
 app({
-  appId: 'WG3M5KTTLD',
-  apiKey: '74e38a958350c6495e632e89c5655c8c',
+  appId: 'FFROL6ZNSV',
+  apiKey: '92f8f4b77b2216972ebaec9954cd666f',
   indexName: 'moduls-canigo'
 });
 
