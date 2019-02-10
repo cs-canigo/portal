0e0d97b898f0520dd40dc525c3cfdb3fabac21a2
Albert <asamo7@gmail.com>
2019-02-10 16:33:09 +0100

    [skip ci] Recursos moguts
    Recursos moguts al repositori propi de l'entorn de desenvolupament gencat/dev-environment

--- static/devenv/nodejs/centos/app.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Prova OK!</h1>\n');
-
-}).listen(process.argv[2]);
