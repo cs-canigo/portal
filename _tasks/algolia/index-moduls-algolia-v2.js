a796ef7fdabc4ff903e7a83596c55c6bd35bb99c
Albert <albertsanchom@gencat.cat>
2023-08-11 11:17:16 +0200

    New version V2 of Canigó dashboard with information about use of the framework (versions, modules)

---
+++ _tasks/algolia/index-moduls-algolia-v2.js
@@ -0,0 +1,141 @@
+var fs = require("fs")
+var algoliasearch = require('algoliasearch');
+
+var client = algoliasearch('SQZ0PDH35B', process.env.ALGOLIA_API_KEY);
+var algolia = client.initIndex('prod_MODULS-CANIGO-V2');
+
+data = readFile("./data/canigoAppsInfoV2.json");
+
+console.log('Processing JSON data ...');
+var _index = processData(JSON.parse(data));
+console.log('... data processed');
+
+//console.log('data: ' + JSON.stringify(_index));
+
+console.log('Saving objects in Algolia index ...');
+saveAlgolia(_index);
+
+console.log('... objects saved');
+
+function processData(data){
+    var packageNames = [];
+    var aplicacio = {};
+    var _versions;
+    var _major;
+    var _minor;
+    var _matches;
+    var _pos;
+
+    for(var i=0,z=data.apps.length;i<z;i++){
+      if(!data.apps[i].repo){continue;}
+
+        for(var w=0,q=data.apps[i].appsInfo.length;w<q;w++){
+          _versions = processVersion(data.apps[i].appsInfo[w].versioCanigo);
+
+          _major = _versions;
+          if(_versions instanceof Array){
+            _major = _versions[0];
+          }
+
+          _minor = _major.slice(1);
+          _major = _major.slice(0,1);
+
+          console.log("_major: " + _major);
+
+          if(_major.startsWith("4") || _major.startsWith("5")) {
+            _major = "3";
+          }
+              
+          if(_minor.indexOf(".")>-1){
+            _matches = _minor.match(/\./g);
+            switch(_matches.length){
+              case 1:
+                _pos = _minor.length;
+                break;
+              case 2:
+                _pos = _minor.lastIndexOf(".");
+                break;
+              case 3:
+                _pos = _minor.indexOf(".", 1);
+                _pos = _minor.indexOf(".",_pos);
+            }
+            _minor = _minor.slice(_minor.indexOf("."), _pos);
+          }
+
+          aplicacio = {
+            'codi' : data.apps[i].repo,
+            'nom' : data.apps[i].appsInfo[w].nomApp,
+            'major' : _major,
+            'minor' : _major + _minor,
+            'rev' : _versions,
+            'modules' : [],
+            'modules_version' : []
+          };
+
+          if(!data.apps[i].appsInfo[w].modulsInfo){
+            packageNames.push(aplicacio);
+            continue;
+		      }
+
+          for(var s=0,t=data.apps[i].appsInfo[w].modulsInfo.length;s<t;s++){
+            aplicacio.modules.push(data.apps[i].appsInfo[w].modulsInfo[s].nomModul);
+			      _versions = processVersion(data.apps[i].appsInfo[w].modulsInfo[s].versioModul);
+			      _major = _versions;
+            if(_versions instanceof Array){
+              _major = _versions[0];
+            }
+            aplicacio.modules_version.push(data.apps[i].appsInfo[w].modulsInfo[s].nomModul+"_"+_major);
+          }
+          packageNames.push(aplicacio);
+        }
+    }
+
+    return packageNames;
+
+}
+
+
+/* processa versió*/
+function processVersion(_version){
+  _version = _version.replace(/\)/g,"]");
+  if(_version.indexOf("[")>-1){
+    _version = _version.replace("[","[\"")
+                       .replace(",","\",\"")
+                       .replace("]","\"]")
+             
+    try{
+      _version = JSON.parse(_version);
+    }catch(e){
+      //...
+    }
+  }
+
+  return _version;
+}
+
+/* Reads file sync  */
+function readFile(file, data){
+  try{
+    return fs.readFileSync(file).toString();
+  }catch(e){
+    return null;
+  }
+}
+
+/* saves data to algolia */
+function saveAlgolia(idx){
+  algolia.clearObjects().then(() => {
+    // done
+
+
+    console.log('Adding...');
+
+    algolia.saveObjects(idx,
+    { autoGenerateObjectIDIfNotExist: true }).then(({ objectIDs }) => {
+      console.log("modules published!");
+      console.log(objectIDs);
+    });
+
+  });
+  
+}
---
+++ content/documentacio/modulsV2.md
@@ -0,0 +1,70 @@
++++
+date        = "2023-08-11"
+title       = "Mòduls i versions Canigó"
+description = ""
+no_index 	= true
++++
+<link href="/css/cercador.css" rel="stylesheet" type="text/css" />
+
+<section class="rslt" id="cercador_text">
+
+<div class="column hidden-xs ">
+	<p id="stats" class="txt_result count_resultats"></p>	
+</div>
+
+<div class="row">
+            <div id="left-column" class="col-xs-12 col-md-9">
+                <div id="major" class="facet col-xs-12 col-md-4"></div>
+                <div id="chart_majors" class="chart col-xs-12 col-md-8"></div>
+                <div id="minor" class="facet col-xs-12 col-md-4"></div>
+                <div id="chart_minors" class="chart col-xs-12 col-md-8"></div>
+                <div id="modules" class="facet col-xs-12 col-md-12"></div>
+                <!--<div id="chart_moduls" class="chart col-xs-12 col-md-5"></div>-->
+                <div id="modules_version" class="facet col-xs-12 col-md-12"></div>
+            </div>
+            <div id="hits" class="list-group col-xs-12 col-md-3">
+            </div>
+            <div class="column paginacio">
+              <div id="pagination"></div>
+            </div>  
+</div>
+
+</section>
+
+<!-- TEMPLATES -->
+<script type="text/html" id="hit-template">
+    <div class="destacat_text list-group-item">
+        <h2>{{codi}} {{nom}} {{rev}}</h2>
+        <div class="app_modules">
+        	{{modules_version}}
+        </div>
+    </div>
+</script>
+
+<script type="text/html" id="no-results-template">
+	<div id="no-results-message">
+	  <p>No s'han trobat resultats per a la cerca <em>"{{query}}"</em>.</p>
+	  <!--a href="." class='clear-all'>Neteja la cerca</a-->
+	</div>
+</script>
+
+<script type="text/html" id="stats-template">
+  S'han trobat <b>{{nbHits}} aplicacions</b>
+</script>
+<!-- /TEMPLATES -->
+
+<div id="logo-algolia">
+	<!--<img src="/images/algolia/Algolia_logo_bg-white.jpg" alt="Logo Algolia" /> -->
+</div>
+
+
+<script src="//cdn.jsdelivr.net/npm/instantsearch.js@1.12.1/dist/instantsearch.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="/js/algolia-search-moduls-V2.js"></script>
+
+<!--
+<script src="//cdn.jsdelivr.net/npm/algoliasearch@4.19.1/dist/algoliasearch.umd.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/instantsearch.js@4.56.9/dist/instantsearch.production.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="/js/algolia-search-moduls-V2.js"></script>
+-->
